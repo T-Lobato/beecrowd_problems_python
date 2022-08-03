7355c3aa11c18da20b7e2d44f4ba2f5af8fa9d7b
Thyago Lobato <thyagollobato@gmail.com>
2022-08-02 22:40:56 -0300

    Média 2

---
+++ 1-iniciante/Ex1006.py
@@ -0,0 +1,7 @@
+#  Variáveis & Entrada
+valor1 = float(input())
+valor2 = float(input())
+valor3 = float(input())
+
+# Processamento & Saída
+print(f'MEDIA = {((valor1 * 2) + (valor2 * 3) + (valor3 * 5))/ 10:.1f}')
